32a250a8141f6ef49589a9959af3949334595ce4
ahmetakyapi <ahmetakyapii@gmail.com>
2021-07-31 18:06:56 +0300

    Add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,31 @@
-import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { NgModule } from '@angular/core'
+import { RouterModule, Routes } from '@angular/router'
+import { AboutComponent } from './about/about.component'
+import { LoginComponent } from './login/login.component'
+import { TodoComponent } from './todo/todo.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginComponent
+  },
+
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'about',
+    component: AboutComponent
+  },
+  {
+    path: 'todo',
+    component: TodoComponent
+  }
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
